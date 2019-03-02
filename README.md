1ada74dec4a9953bd900b67d5c380e5343c0f00c
Myron Turner <turnermm02@shaw.ca>
2019-03-02 08:02:28 -0600

    Removes nodisp text when a page is accessed through Dokuwiki's view source action

---
+++ README.md
@@ -0,0 +1,7 @@
+# nodisp
+This plugin enables concealment of text placed between the nodisp tags:
+     ``<nodisp n>. . .</nodisp>``
+``n`` is an acl level.  Any acl permission less then ``n`` is
+denied access to the marked off text.  This text cannot be viewed in the
+browser by means of view source because it is not sent from the server 
+to the browser.     
--- action.php
+++ action.php
@@ -6,13 +6,28 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 class action_plugin_nodisp extends DokuWiki_Action_Plugin {
-   	private   $accumulator = null;
-    private $current;
     public function register(Doku_Event_Handler $controller) {   
        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, 'handle_wiki_content'); 	    
     }
 
   function handle_wiki_content(Doku_Event $event, $param) {    
+    global $ACT;
+    $act = act_clean($ACT);
+        if($act == 'source') {      
+           $event->data = preg_replace_callback( 
+            '|&lt;nodisp (\d+)&gt;.*?&lt;\/nodisp&gt;|ms',
+            function($matches) {     
+               global $ID;
+               $acl = auth_quickaclcheck($ID);
+               if($acl < $matches[1]) {
+                   return "";
+               }          
+               return $matches[0];
+            },$event->data
+         ) ;
+          return;
+      }
+        
      $event->data = preg_replace_callback( 
         '|\<div class\s+=\s+"nodisp_(\d+)">.*?\<\/div>|ms',
         function($matches) {     
