795ba7e0346a9808ff8e57c63ed8063ea61b1843
Myron Turner <turnermm02@shaw.ca>
2021-07-27 13:29:18 -0500

    Added check for syntax.php in plugin's root directory with msg to remove

--- action.php
+++ action.php
@@ -5,11 +5,20 @@
 */
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
+ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 class action_plugin_nodisp extends DokuWiki_Action_Plugin {
     public function register(Doku_Event_Handler $controller) {   
        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, 'handle_wiki_content'); 	    
     }
 
+  function __construct() {
+      
+      if(file_exists(DOKU_PLUGIN . 'nodisp/syntax.php')) {
+               msg("Please remove syntax.php from lib/plugins/nodisp or remove the plugin and reinstall");
+      }
+     
+  }
+  
   function handle_wiki_content(Doku_Event $event, $param) {    
     global $ACT;
     $act = act_clean($ACT);
